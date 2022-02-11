3cc8d2cd909be6a0bf9fb009acf9754d08c8d8e9
Nelson M <mwinel@live.com>
2022-02-12 00:50:37 +0300

    Add font family

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 export default function Home() {
     return (
-        <h1 className="text-3xl font-bold underline text-gray-800">
-            Todo App
+        <h1 className="text-4xl font-bold text-gray-800">
+            TODO
         </h1>
     );
 }
